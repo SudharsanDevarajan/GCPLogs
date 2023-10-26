524e947ee22be8e30af2b286afc056b289dea7f9
Sudharsan D <devarajsudharsan@gmail.com>
2023-10-26 14:26:48 +0530

    GCP logs public access

--- Sources/GCPLogs/GCPLogs.swift
+++ Sources/GCPLogs/GCPLogs.swift
@@ -1,2 +1,47 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public class GCPLogs{
+    @available(iOS 13.0.0, *)
+    public static func save(config: GCPConfigure,jsonPayload: [String: Any], forToken: String) async throws{
+        let params : [String:Any] = [
+            "entries": [
+                [
+                    "logName": "projects/\(config.projectID)/logs/my-test-log",
+                    "resource": [
+                        "labels": [
+                            "projectId": config.projectID,
+                            "instance_id":"api",
+                            "zone":"india"
+                        ],
+                        "type": "gce_instance"
+                    ],
+                    "severity": "INFO",
+                    "jsonPayload": jsonPayload
+                ]
+            ],
+            "logName": "projects/\(config.projectID)/logs/my-test-log"
+        ]
+        
+        do{
+            try await Networking.shared.logData(config.gcpUrl, params, forToken)
+        }catch{
+            throw error
+        }
+    }
+}
+
+
+public struct GCPConfigure{
+    let gcpUrl: String
+    let projectID: String
+    let logType: LogType
+}
+
+
+enum LogType{
+    case request
+    case response
+    case other(detail: String)
+}
--- Sources/GCPLogs/Networking.swift
+++ Sources/GCPLogs/Networking.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+
+
+class Networking{
+    
+    static let shared = Networking()
+    
+    private init(){}
+    
+    @available(iOS 13.0.0, *)
+    func logData(_ url: String,_ body: [String:Any],_ token: String) async throws{
+        do{
+            let request = try createRequest(url, token, body)
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let json = try JSONSerialization.jsonObject(with: data)
+            print("GCPLogs success \(json)")
+        }catch NetworkingError.invalidURL{
+            throw NetworkingError.invalidURL
+        }catch NetworkingError.invalidRequest{
+            throw NetworkingError.invalidRequest
+        }catch{
+            throw NetworkingError.gcpLoginFailed
+        }
+    }
+    
+    private func createRequest(_ urlString: String,_ userToken: String, _ body: [String: Any]) throws -> URLRequest{
+        
+        guard let url = URL(string: urlString) else {
+            throw NetworkingError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue(HttpHeader.contentTypeValue, forHTTPHeaderField: HttpHeader.contentTypeKey)
+        request.setValue("\(HttpHeader.bearer) \(userToken)", forHTTPHeaderField: HttpHeader.auth)
+        request.httpMethod = HttpMethod.post
+        
+        do{
+            request.httpBody = try JSONSerialization.data(withJSONObject: body, options: .prettyPrinted)
+        }catch{
+            throw NetworkingError.invalidRequest
+        }
+        
+        return request
+    }
+}
+
+struct HttpHeader{
+    static let contentTypeKey = "Content-Type"
+    static let contentTypeValue = "application/json"
+    static let auth = "Authorization"
+    static let bearer = "Bearer"
+}
+
+
+struct HttpMethod{
+    static let get = "GET"
+    static let post = "POST"
+    static let put = "PUT"
+}
+
+
+enum NetworkingError: Error{
+    case invalidURL
+    case invalidRequest
+    case gcpLoginFailed
+}
